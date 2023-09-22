d0a4b6143fda60b01fa58995fac3ae6d44f99267
Manul Thanura <manulkarunarathna@gmail.com>
2023-09-22 06:42:37 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,35 @@
 import '@styles/globals.css'
 
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+import Footer from '@components/Footer';
+
 export const metadata = {
   title: 'Tailwind CSS components',
   description: 'A free repository of open source Tailwind CSS components and templates to bootstrap your new apps, projects or landing sites!',
 }
 
-export default function RootLayout({ children }) {
+const Rootlayout = ({children}) => {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <link rel="icon" type="image/svg" href="/assets/images/logo.svg" />
+      </head>
+      <body className="no-scrollbar overflow-y-auto">
+          <Provider>
+            <div className="main">
+                <div className="gradient" />
+            </div>
+
+            <main className="app">
+              <Nav />
+              {children}
+              <Footer />
+            </main>
+          </Provider>
+      </body>
     </html>
   )
 }
+
+export default Rootlayout
