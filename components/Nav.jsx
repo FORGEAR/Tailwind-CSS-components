0c4c229c6a66c4c4c0b6139100a08bd500360f43
Manul Thanura <manulkarunarathna@gmail.com>
2023-09-22 07:47:22 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -81,10 +81,10 @@ const Nav = () => {
         </div>
     </div>
 
-    <!-- Mobile menu, show/hide based on menu state. -->
+    {/* <!-- Mobile menu, show/hide based on menu state. --> */}
     <div class="sm:hidden" id="mobile-menu">
         <div class="space-y-1 px-2 pb-3 pt-2">
-        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+        {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
         <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
         <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
         <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
