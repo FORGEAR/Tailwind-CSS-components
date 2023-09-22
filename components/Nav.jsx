0dcc3d45ae633b506baa97da7d601435c5da3214
Manul Thanura <manulkarunarathna@gmail.com>
2023-09-22 06:43:30 +0530

    Update Nav.jsx

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -3,7 +3,7 @@
 import Link from 'next/link' ;
 import Image from 'next/image' ;
 import { useState, useEffect } from 'react';
-import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
+// import {signIn, signOut, useSession, getProviders} from 'next-auth/react'
 
 const Nav = () => {
   const {data: session} = useSession();
