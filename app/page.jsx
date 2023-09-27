8ae63c3ec53d710ff5c4fcd077aef1bb46dce699
Manul Thanura <manulkarunarathna@gmail.com>
2023-09-27 19:02:26 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -22,15 +22,6 @@ const Home = () => {
   {/* <Feed /> */}
   </section>
   
-    // <section className="w-full flex-center flex-col">
-    //     <h1 className="head_text text-center">Welcome to the AI Prompt Ideas Community!
-    //     <br className="max-md:hidden"/>
-    //     <span className="emerald_gradient text-center">The place where people can share and discuss AI prompt ideas</span>
-    //     </h1>
-    //     <p className="desc text-center">This app is deployed on Vercel and is using the </p>
-
-    //     <Feed />
-    // </section>
   )
 }
 
