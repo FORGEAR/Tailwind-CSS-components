45a402cb4dd74a1bf9dc9d5aad681f86c203b893
Manul Thanura <manulkarunarathna@gmail.com>
2023-09-29 12:44:44 +0530

    Update Feed.jsx

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -10,19 +10,16 @@ const features = [
     name: 'SSL certificates',
     description:
       'Sit quis amet rutrum tellus ullamcorper ultricies libero dolor eget. Sem sodales gravida quam turpis enim lacus amet.',
-    icon: LockClosedIcon,
   },
   {
     name: 'Simple queues',
     description:
       'Quisque est vel vulputate cursus. Risus proin diam nunc commodo. Lobortis auctor congue commodo diam neque.',
-    icon: ArrowPathIcon,
   },
   {
     name: 'Advanced security',
     description:
       'Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt mattis aliquet hac quis. Id hac maecenas ac donec pharetra eget.',
-    icon: FingerPrintIcon,
   },
 ]
 
