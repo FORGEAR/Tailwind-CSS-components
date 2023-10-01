83e35070ceec1f34adbc1fb2576bb9cfe808900d
Manul Thanura <manulkarunarathna@gmail.com>
2023-10-01 10:56:48 +0530

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,7 +12,7 @@ const blogCollection = defineCollection({
       alt: z.string(),
     }),
     publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
+    author: z.string().default('FORGEAR'),
     category: z.string(),
     tags: z.array(z.string()),
   }),
