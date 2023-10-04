d8bef645743fece9d530520a1d803784aa7fa545
Manul Thanura <manulkarunarathna@gmail.com>
2023-10-04 06:29:41 +0530

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -31,9 +31,23 @@ const teamCollection = defineCollection({
   }),
 });
 
+const compCollection = defineCollection({
+  schema: z.object({
+    draft: z.boolean(),
+    name: z.string(),
+    title: z.string(),
+    avatar: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    publishDate: z.string().transform(str => new Date(str)),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
   'blog': blogCollection,
   'team': teamCollection,
+  'comp': compCollection,
 };
