ce08f1f48eadbb070acfe25c705da43386b30285
Manul Thanura <manulkarunarathna@gmail.com>
2023-10-06 11:33:09 +0530

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -36,6 +36,7 @@ const compCollection = defineCollection({
     draft: z.boolean(),
     name: z.string(),
     title: z.string(),
+    
     avatar: z.object({
       src: z.string(),
       alt: z.string(),
